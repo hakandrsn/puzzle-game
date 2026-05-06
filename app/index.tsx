497e2e022af76c4450575588772135aed7a0c32a
Hakan Dursun <68669489+hakandrsn@users.noreply.github.com>
2026-05-06 22:23:04 +0300

    index

--- app/index.tsx
+++ app/index.tsx
@@ -62,7 +62,7 @@ export default function StartScreen() {
 
   const hasSeenOnboarding = useHasSeenOnboarding();
   const onboardingHydrated = useOnboardingHydrated();
-
+  console.log("")
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
